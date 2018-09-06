19d51be61321e98d4334746bd009d6872edcec81
Stadnik Andrii <astadnik@student.unit.ua>
2018-09-07 00:22:25 +0300

    Updated variant. I can check things one more time, but it's cool already

--- ex1.py
+++ ex1.py
@@ -3,11 +3,16 @@ from Display import draw
 
 if __name__ == '__main__':
     if len(sys.argv) == 1:
-        print('Please give some parameters')
+        print('Usage: python3.6 ex1.py PATH')
     else:
         try:
             with open(sys.argv[1], 'r') as fd:
-                text = fd.readlines()
-            draw(text)
+                text = [l.strip() for l in fd.readlines()]
         except FileNotFoundError:
             print('File not found')
+            quit()
+        try:
+            draw(text)
+        except RuntimeError as e:
+            print(repr(e))
+            quit()
