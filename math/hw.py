70ec2ceae3d0e0c0d34d255bfd08423e94735fa8
Stadnik Andrii <astadnik@student.unit.ua>
2018-12-02 21:08:53 +0200

    Added regression with more features

--- math/hw.py
+++ math/hw.py
@@ -1,5 +1,6 @@
 from univ_statistics import info, relations
 from data import dir_met, indir_met
+from sklearn import linear_model
 
 import matplotlib.pyplot as plt
 
@@ -125,7 +126,6 @@ def display_regression():
                                                                           3))
         ax[i % 3][i // 3].plot([m2 * x + k2 for x in range(-1, 3)], range(-1,
                                                                           3))
-
     plt.tight_layout()
     fig_dir.show()
     plt.waitforbuttonpress()
@@ -137,9 +137,56 @@ def part2():
     display_regression()
 
 
+def part3():
+    """Trying to make more features
+    :returns: TODO
+
+    """
+    keys = [
+        ["LOC", "NOM"],
+        ["LOC", "HIT"],
+        ["LOC", "ATFD"],
+        ["NOM", "HIT"],
+        ["NOP", "WMC"]
+    ]
+    met = {**dir_met, **indir_met}
+    fig_dir, ax = plt.subplots(3, 2, figsize=(10, 10))
+    fig_dir.canvas.set_window_title("Spearman correlation")
+    for i, k in enumerate(keys):
+        key1, key2 = k
+        vals1 = met[key1]
+        stat1 = info(vals1)
+        vals1 = [(val - stat1["mean"]) / stat1["std"] for val in vals1]
+        vals2 = met[key2]
+        stat2 = info(vals1)
+        vals2 = [(val - stat2["mean"]) / stat2["std"] for val in vals2]
+        ax[i % 3][i // 3].scatter(range(len(vals1)), vals1, label=key1)
+        ax[i % 3][i // 3].scatter(range(len(vals2)), vals2, label=key2)
+        ax[i % 3][i // 3].legend()
+        features = [[x, x ** 2, x ** 3] for x in range(len(vals1))]
+        regr1 = linear_model.LinearRegression()
+        regr1.fit(features, vals1)
+        pred1 = [regr1.predict([[x, x ** 2, x ** 3]]) for x in
+                 range(len(vals1))]
+        regr2 = linear_model.LinearRegression()
+        regr2.fit(features, vals2)
+        pred2 = [regr2.predict([[x, x ** 2, x ** 3]]) for x in
+                 range(len(vals2))]
+        m1, k1 = relations(vals1, range(len(vals1)))["lsq"]
+        m2, k2 = relations(vals2, range(len(vals2)))["lsq"]
+        ax[i % 3][i // 3].plot(range(len(vals1)), pred1)
+        ax[i % 3][i // 3].plot(range(len(vals2)), pred2)
+
+    plt.tight_layout()
+    fig_dir.show()
+    plt.waitforbuttonpress()
+    plt.close()
+
+
 def main():
     part1()
     part2()
+    part3()
 
 
 if __name__ == "__main__":
