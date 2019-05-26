bac48a654e549137acae24a55a5292734f38ad8d
Andrii Stadnik <andrii.stadnik@globallogic.com>
2019-05-26 18:50:42 +0300

    Next version of the homework

--- sem_2/EMPI/main_window.py
+++ sem_2/EMPI/main_window.py
@@ -2,56 +2,91 @@
 This is the main window which contains all objects needed for interacting with
 the program
 """
-from PySide2 import QtCore, QtGui, QtWidgets
+from PySide2 import QtCore, QtGui, QtWidgets, QtWebEngineWidgets
 from project import Project
+import desc
 
 # from PySide2.QtCore import Qt, Slot
 # from PySide2.QtWidgets import (QLabel, QPushButton, QVBoxLayout, QWidget)
 
-class MyWidget(QtWidgets.QWidget):
+class MyWidget(QtWidgets.QDialog):
     """This is the main widget"""
     def __init__(self):
-        QtWidgets.QWidget.__init__(self)
+        QtWidgets.QDialog.__init__(self)
 
-        # self.process = self.createButton("Process", self.open_proj)
-        self.text = QtWidgets.QTextEdit("Hello World")
-        self.text.setAlignment(QtCore.Qt.AlignCenter)
+        #################
+        #  Add widgets  #
+        #################
+        
+        self.desc = QtWidgets.QTextEdit("Please open the project")
+        self.desc.setReadOnly(True)
+
+        self.total = QtWidgets.QLabel('Total')
+        self.total.setEnabled(False)
+
         self.list_metric = QtWidgets.QComboBox()
         self.list_metric.setEnabled(False)
         self.list_metric.currentTextChanged.connect(self.change_metrics)
 
-        self.menu_bar = QtWidgets.QMenuBar(self)
-        self.file_menu = self.menu_bar.addMenu("&File")
-        self.file_menu.addAction("&Open project", self.open_proj)
+        self.proj = self.createButton('Open project', self.open_proj)
 
-        self.layout = QtWidgets.QVBoxLayout()
-        self.layout.addWidget(self.text)
-        # self.layout.addWidget(self.process)
-        self.layout.addWidget(self.list_metric)
+        ####################
+        #  Set the layout  #
+        ####################
+        self.web = QtWebEngineWidgets.QWebEngineView()
+        self.web.setEnabled = False
+        self.web.load(None)
+        # self.web.load(QtCore.QUrl.fromLocalFile('/home/amyznikov/astadnik/univ/univ_oop/sem_2/EMPI/LOC.html'))
+
+        self.layout = QtWidgets.QGridLayout()
+
+        self.buttons = QtWidgets.QVBoxLayout()
+        self.buttons.addStretch()
+        self.buttons.addWidget(self.desc)
+        self.buttons.addWidget(self.total)
+        self.buttons.addWidget(self.proj)
+        self.buttons.addWidget(self.list_metric)
+
+        # self.layout.addWidget(self.text, 0, 0)
+        self.layout.addWidget(self.web, 0, 0)
+        self.layout.addLayout(self.buttons, 0, 1)
         self.setLayout(self.layout)
 
-        self.open_proj()
+        # self.open_proj()
 
     def change_metrics(self):
-        self.text.setText('\n'.join(['{}: {}'.format(name, loc)
-                                     for name, loc in self.project.metrics[
-                                         self.list_metric.currentText()].items()]))
-        
+        self.web.load(QtCore.QUrl.fromLocalFile('/home/amyznikov/astadnik/univ/univ_oop/sem_2/EMPI/{}.html'.format(self.list_metric.currentText())))
+        self.desc.setText(desc.metrics[self.list_metric.currentText()])
+        self.total.setText('Total: {}'.format(self.project.metrics['total'][self.list_metric.currentText()]))
+        # self.text.setText('\n'.join(['{}: {}'.format(name, loc)
+        #                              for name, loc in self.project.metrics[
+        #                                  self.list_metric.currentText()].items()]))
 
     def open_proj(self):
         # path = QtWidgets.QFileDialog.getExistingDirectory(self, "Find Files",
         #         "/home/amyznikov/astadnik/univ/Simple-Java-Text-Editor/")
                 # QtCore.QDir.currentPath())
-        path = "/home/amyznikov/astadnik/univ/jadx/"
+        path = "/home/amyznikov/astadnik/univ/Simple-Java-Text-Editor/"
+        print(path)
         try:
             self.project = Project(path)
         except RuntimeError as e:
             print(e)
+            self.desc = QtWidgets.QTextEdit("Please open the project")
+            self.web.setEnabled(False)
             self.list_metric.setEnabled(False)
+            self.total.setEnabled(False)
             self.list_metric.clear()
         self.project.compute_metrics()
         self.list_metric.setEnabled(True)
-        self.list_metric.addItems(list(self.project.metrics.keys()))
+        self.web.setEnabled = True
+        self.total.setEnabled(True)
+        self.list_metric.clear()
+        metrics = list(self.project.metrics.keys())
+        metrics.remove('total')
+        self.list_metric.addItems(metrics)
+        self.list_metric.setCurrentIndex(0)
+        self.change_metrics()
 
 
     def createButton(self, text, member):
--- sem_2/EMPI/project.py
+++ sem_2/EMPI/project.py
@@ -1,6 +1,9 @@
 """This module handles opening the project"""
 
 from PySide2 import QtCore
+import plotly.graph_objs as go
+from plotly.offline import plot
+from itertools import permutations
 import re
 
 class Project:
@@ -11,6 +14,8 @@ class Project:
         self.files = self.parse_files()
         if not self.files:
             raise RuntimeError("No .java files were found")
+        self.compute_metrics()
+        self.make_plots()
 
     def find_java_files(self, path):
         """This function recursively finds java files in the path"""
@@ -38,15 +43,20 @@ class Project:
 
     def compute_metrics(self):
         self.metrics = {}
+        self.metrics['total'] = {}
         self.calc_LOC()
         self.calc_NOC()
         self.calc_NDD()
         self.calc_CALL()
         self.calc_NOM()
+        self.calc_WMC()
+        self.calc_TCC()
+        self.calc_PNAS()
 
     def calc_LOC(self):
         self.metrics['LOC'] = {name[1]:len(content) for
                                name, content in zip(self.files_p, self.files)}
+        self.metrics['total']['LOC'] = sum(self.metrics['LOC'].values())
 
     def calc_NOC(self):
         self.metrics['NOC'] = {name[1]:[s.strip().split('//')[0] for s in content
@@ -54,12 +64,14 @@ class Project:
                                             re.match('.* class .*', s.strip().split('//')[0]) and
                                             not re.match('.*[";].*', s.split('//')[0]))]
                                for name, content in zip(self.files_p, self.files)}
+        self.metrics['total']['NOC'] = sum(map(len, self.metrics['NOC'].values()))
 
     def calc_NDD(self):
         self.metrics['NDD'] = {name[1]:len([rez
                                             for s in self.metrics['NOC'][name[1]]
                                             for rez in re.findall('implements|extends', s)])
                                for name in self.files_p}
+        self.metrics['total']['NDD'] = sum(self.metrics['NDD'].values())
 
     def calc_CALL(self):
         self.metrics['CALL'] = {name[1]:[rez
@@ -67,6 +79,7 @@ class Project:
                                          for rez in re.findall('[\w+\|\)]\.\w+\(',
                                                                s.strip().split('//')[0])]
                                 for name, content in zip(self.files_p, self.files)}
+        self.metrics['total']['CALL'] = sum(map(len, self.metrics['CALL'].values()))
 
     def calc_NOM(self):
         self.metrics['NOM'] = {name[1]:[s.strip().split('//')[0] for s in content
@@ -74,4 +87,65 @@ class Project:
                                             re.match('.* {.*', s.strip().split('//')[0]) and
                                             not re.match('.*class.*', s.split('//')[0]))]
                                for name, content in zip(self.files_p, self.files)}
+        self.metrics['total']['NOM'] = sum(map(len, self.metrics['NOM'].values()))
 
+    def calc_WMC(self):
+        self.metrics['WMC'] = {name[1]:len([len(re.findall('if|while|for|case|else', s)) + 1
+                                            for s in content
+                                            if not s.strip().startswith(('//', '*'))])
+                               for name, content in zip(self.files_p, self.files)}
+        self.metrics['total']['WMC'] = sum(self.metrics['WMC'].values())
+
+    def calc_TCC(self):
+        for f, cont in zip(self.files_p, self.files):
+            f = f[1]
+            classes = {cl: {} for cl in self.metrics['NOC'][f]}
+            rez = 0
+            for cl, cl_meths in classes.items():
+                for meth in self.metrics['NOM'][f]:
+                    indent = None
+                    for line in cont:
+                        if indent:
+                            if len(line) - len(line.lstrip()) <= indent:
+                                break
+                            cl_meths[f] |= set(re.findall('self.\w\+', line))
+                        if meth in line:
+                            indent = len(line) - len(line.lstrip())
+                            cl_meths[f] = set()
+                    else:
+                        continue
+                    break
+                con_cl = 0
+                print(cl)
+                for perm in permutations(cl_meths, 2):
+                    if perm[0][1] & perm[1][1]:
+                        con_cl += 1
+                rez = (rez + (con_cl / (len(cl[1]) * (len(cl[1]) - 1)))) / 2
+            self.metrics['TCC'][f] = rez
+
+    def calc_PNAS(self):
+        pass
+
+    def make_plots(self):
+        layout = go.Layout(showlegend=False)
+        plot(go.Figure(data=[go.Pie(labels=list(self.metrics['LOC'].keys()), 
+                                    values=list(self.metrics['LOC'].values()), textinfo='none')],
+                       layout=layout), auto_open=False, filename='LOC.html')
+        plot(go.Figure(data=[go.Pie(labels=list(self.metrics['NOC'].keys()), 
+                                    values=list(map(len, self.metrics['NOC'].values())),
+                                    textinfo='none')],
+                       layout=layout), auto_open=False, filename='NOC.html')
+        plot(go.Figure(data=[go.Pie(labels=list(self.metrics['NOM'].keys()), 
+                                    values=list(self.metrics['NOM'].values()), textinfo='none')],
+                       layout=layout), auto_open=False, filename='NOM.html')
+        plot(go.Figure(data=[go.Pie(labels=list(self.metrics['NDD'].keys()), 
+                                    values=list(self.metrics['NDD'].values()), textinfo='none')],
+                       layout=layout), auto_open=False, filename='NDD.html')
+        plot(go.Figure(data=[go.Pie(labels=list(self.metrics['CALL'].keys()), 
+                                    values=list(map(len, self.metrics['CALL'].values())),
+                                    textinfo='none')],
+                       layout=layout), auto_open=False, filename='CALL.html')
+        plot(go.Figure(data=[go.Pie(labels=list(self.metrics['NOM'].keys()), 
+                                    values=list(map(len, self.metrics['NOM'].values())),
+                                    textinfo='none')],
+                       layout=layout), auto_open=False, filename='NOM.html')
